dfff7a1ccd7494ed2e2fd43e0090ca9efeccb336
kamil-imielowski <kamil.imielowski@ctiplus.pl>
2017-11-25 13:05:09 +0100

    composer

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+require_once dirname(__FILE__).'/vendor/autoload.php';
+include_once dirname(__FILE__).'/displayErrors.php';
+require_once dirname(__FILE__).'/functions/cityForMap.php';
+
+$translate = new classes\Languages\Translate($_COOKIE['lang']);
+
+#countries for map
+$countriesForMap = getCountriesForMap();
+
+if(isset($_POST['action']) || isset($_GET['action'])){
+	$action = isset($_POST['action']) ? $_POST['action'] : $_GET['action'];
+
+    switch ($action) {
+        case 'value':
+            # code...
+            break;
+    }
+}else{
+    include dirname(__FILE__).'/templates/index.html.php';
+}
+
+
+?>
